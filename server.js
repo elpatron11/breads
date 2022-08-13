a75167b298f5b33f5d9345f8ca0ec0c1e26d6759
unknown <cabaraul@gmail.com>
2022-08-13 10:02:45 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 // DEPENDENCIES
 const express = require('express')
 
@@ -13,7 +11,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
+app.use(express.static('public'))
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -24,9 +22,15 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
 
 
